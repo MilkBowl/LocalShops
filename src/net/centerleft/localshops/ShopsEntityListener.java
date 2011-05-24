1ebe26afbabfbce8c27453fae4d6b187c2606085
sleaker <sleaker@sleak-pc>
2011-05-25 01:34:22 +0800

    Fixed EntityListener shop detection for sign breakage.

--- src/net/centerleft/localshops/ShopsEntityListener.java
+++ src/net/centerleft/localshops/ShopsEntityListener.java
@@ -22,38 +22,37 @@ public class ShopsEntityListener extends EntityListener {
     public ShopsEntityListener(LocalShops plugin) {
         this.plugin = plugin;
     }
-    
-    
+
+
     public void onEntityExplode(EntityExplodeEvent event) {
-        Shop shop = null;
         if (event.isCancelled()) {
             return;
         }
+
         //Check the blocks to see if we need to remove a sign from the shopdata
         for (Block block : event.blockList()) {
+            Location blockLoc = block.getLocation();
             //Skip checking this block if it's not a sign
             if (block.getType() != Material.WALL_SIGN && block.getType() != Material.SIGN_POST ) {
                 continue;
             }
-            
-            //Search for Shops near event block and add them to the array list.
-            shop = plugin.getShopManager().getShop(block.getWorld().getName(), block.getX(), block.getY(), block.getZ());
 
+            //Search for shop at block location
+            Shop shop = plugin.getShopManager().getShop(blockLoc);
+            //If no shop found at the location skip to the next block
             if (shop == null) {
-                return;
-            }
-            
-            Location blockLoc = block.getLocation();
-            Iterator<Location> iter = shop.getSignMap().keySet().iterator();
-            while (iter.hasNext()) {
-                Location signLoc = iter.next();
-                if ( signLoc.getBlockX() == blockLoc.getBlockX() && signLoc.getBlockY() == blockLoc.getBlockY() && signLoc.getBlockZ() == blockLoc.getBlockZ()) {
-                    iter.remove();
-                    //Only 1 match possible per block - Break loop if we found one.
-                    break;
+                continue;
+            } else {
+                Iterator<Location> iter = shop.getSignMap().keySet().iterator();
+                while (iter.hasNext()) {
+                    Location signLoc = iter.next();
+                    if ( signLoc.getBlockX() == blockLoc.getBlockX() && signLoc.getBlockY() == blockLoc.getBlockY() && signLoc.getBlockZ() == blockLoc.getBlockZ()) {
+                        //Remove the object from the map and Only 1 match possible per block so Break loop if we found one.
+                        iter.remove();
+                        break;
+                    }
                 }
             }
         }
     }
-    
 }
