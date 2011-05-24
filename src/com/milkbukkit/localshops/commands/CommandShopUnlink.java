60a345d3b7c5fecde76014f65e06fe51a4819207
sleaker <sleaker@sleak-pc>
2011-05-25 06:01:18 +0800

    force merge with updated unlink logic - MERGE MERGE MERGE!

--- src/com/milkbukkit/localshops/commands/CommandShopUnlink.java
+++ src/com/milkbukkit/localshops/commands/CommandShopUnlink.java
@@ -3,10 +3,14 @@
  */
 package com.milkbukkit.localshops.commands;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.milkbukkit.localshops.Config;
 import com.milkbukkit.localshops.LocalShops;
 import com.milkbukkit.localshops.Shop;
 
@@ -34,9 +38,25 @@ public class CommandShopUnlink extends Command {
                 sender.sendMessage(LocalShops.CHAT_PREFIX + ChatColor.DARK_AQUA + "You don't have permission to use this command");
                 return true;
             }
-            // Get player
-            Player player = (Player) sender;
+            Pattern pattern = Pattern.compile("(?i)link\\s+(.*)");
+            Matcher matcher = pattern.matcher(command);
+            if (matcher.find()) {
+                String worldName = matcher.group(0);
+                shop = plugin.getShopManager().getShop(Config.getGlobalShopUuid(worldName));
+                if (shop == null) {
+                    sender.sendMessage("No global shop on " + worldName + " to unlink!");
+                    return true;
+                } else {
+                    Config.globalShopsRemove(worldName);
+                    sender.sendMessage(ChatColor.DARK_AQUA + "Unlinked " + ChatColor.WHITE + shop.getName() + ChatColor.DARK_AQUA + " from " + ChatColor.WHITE + worldName);
+                }
+            }
+        } else {
+            sender.sendMessage("Console is not implemented yet.");
+            return true;
         }
+        // Show link help
+        sender.sendMessage(ChatColor.WHITE + "   /" + commandLabel + " unlink [worldname] " + ChatColor.DARK_AQUA + "- Link a global shop to this world.");
         
         return false;
     }
