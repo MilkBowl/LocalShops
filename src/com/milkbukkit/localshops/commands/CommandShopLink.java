122c7d4565e019385846172cf29f4658ba92aef0
sleaker <sleaker@sleak-pc>
2011-05-25 06:58:55 +0800

    Added '/gshop link <id> world' detection

--- src/com/milkbukkit/localshops/commands/CommandShopLink.java
+++ src/com/milkbukkit/localshops/commands/CommandShopLink.java
@@ -43,8 +43,35 @@ public class CommandShopLink extends Command {
             // Get player
             Player player = (Player) sender;
 
-            Pattern pattern = Pattern.compile("(?i)link\\s+(.*)");
+            Pattern pattern = Pattern.compile("(?i)link\\s+([A-Za-z0-9\\-]+)\\s+(\\w+)$");
             Matcher matcher = pattern.matcher(command);
+            if (matcher.find()) {
+                String worldName = matcher.group(1);
+                String key = matcher.group(0);
+                shop = plugin.getShopManager().getShop(key);
+                if (shop == null) {
+                    sender.sendMessage("Could not find a shop that matches id: " + key);
+                    return true;
+                } else {
+                    if (shop.isGlobal()) {
+                        if (Config.globalShopsContainsKey(worldName)) {
+                            sender.sendMessage(worldName + " already has a global shop with id: " + plugin.getShopManager().getShop(Config.getGlobalShopUuid(worldName)).getShortUuidString());
+                            return true;
+                        } else {
+                            Config.globalShopsAdd(worldName, shop.getUuid());
+                            sender.sendMessage("Added " + shop.getName() + " as a global shop for " + worldName);
+                            return true;
+                        }
+                    } else {
+                        sender.sendMessage("Shop with id " + key + " is a global shop. Unable to link to a world");
+                        return true;
+                    }
+                }
+            }
+
+            matcher.reset();
+            pattern = Pattern.compile("(?i)link\\s+(.+)");
+            matcher = pattern.matcher(command);
             if (matcher.find()) {
                 String worldName = matcher.group(0);
                 shop = getCurrentShop(player);
@@ -75,7 +102,8 @@ public class CommandShopLink extends Command {
             return true;
         }
         // Show link help
-        sender.sendMessage(ChatColor.WHITE + "   /" + commandLabel + " link [worldname] " + ChatColor.DARK_AQUA + "- Link a global shop to this world.");
+        sender.sendMessage(ChatColor.WHITE + "   /" + commandLabel + " link [worldname] " + ChatColor.DARK_AQUA + "- Link a global shop from this world to worldname");
+        sender.sendMessage(ChatColor.WHITE + "   /" + commandLabel + " link [shopid] [worldname] " + ChatColor.DARK_AQUA + "- Link a global shop with id to worldname");
         return false;
     }
 
