a759078375fe09f16e005c948c3a2a40d93e51e8
Morgan <mrh9724@lanaddict.com>
2011-05-25 10:29:05 -0400

    DynamicThread should use vars from the Config class

--- src/com/milkbukkit/localshops/threads/DynamicThread.java
+++ src/com/milkbukkit/localshops/threads/DynamicThread.java
@@ -9,8 +9,6 @@ import com.milkbukkit.localshops.InventoryItem;
 import com.milkbukkit.localshops.LocalShops;
 import com.milkbukkit.localshops.Shop;
 
-
-
 /*
  * Represents the Dynamic Shop Task which is run in a thread
  * 
@@ -19,27 +17,16 @@ import com.milkbukkit.localshops.Shop;
  * 
  */
 public class DynamicThread extends Thread {
-    
+
     private boolean run = true;
-    private int dynamicInterval = 0;            // Interval default is 900.  Minimum is 300.
-    private int dynamicChance = 0;              // Chance for an item to actually change price.
-    private int maxPriceChange = 0;             // Max price change that can occur. Minimum is 0.
-    private int minPriceChange = 0;             // Min price change that can occur. Minimum is 0;
+    private LocalShops plugin = null;
     private static Random randNum = new Random();
-    
-    private Collection<Shop> shops;
 
     // Logging
     private static final Logger log = Logger.getLogger("Minecraft");
 
     public DynamicThread(LocalShops plugin) {
-        
-        shops = plugin.getShopManager().getAllShops();
-             
-        this.dynamicInterval = Config.getDynamicInterval();
-        this.dynamicChance = Config.getDynamicChance();
-        this.maxPriceChange = Config.getDynamicMaxPriceChange();
-        this.minPriceChange = Config.getDynamicMinPriceChange();
+        this.plugin = plugin;
     }
 
     public void setRun(boolean run) {
@@ -50,7 +37,12 @@ public class DynamicThread extends Thread {
         while (run) {
             // Do our Sleep stuff!
 
-            for (int i = 0; i < dynamicInterval; i++) {
+            int interval = Config.getDynamicInterval();
+            int dynamicChance = Config.getDynamicChance();
+            int maxPriceChange = Config.getDynamicMaxPriceChange();
+            int minPriceChange = Config.getDynamicMinPriceChange();
+
+            for (int i = 0; i < interval; i++) {
                 try {
                     if (!run) {
                         return;
@@ -61,50 +53,51 @@ public class DynamicThread extends Thread {
                 }
             }
 
-            for (Shop shop : shops) {
+            for (Shop shop : plugin.getShopManager().getAllShops()) {
                 if (shop.isDynamicPrices()) {
                     // Log to let us know scheduler is working.
                     log.info("[LocalShops] - Adjusting prices for shop: " + shop.getName());
 
                     int totDynItems = shop.numDynamicItems();
-                    //Ignore this shop if we have no items set to Dynamic
+                    // Ignore this shop if we have no items set to Dynamic
                     if (totDynItems == 0) {
                         log.info("[LocalShops] - Shop " + shop.getName() + " is set to dynamic but has no dynamic items");
                         continue;
                     }
 
-                    for ( InventoryItem item : shop.getItems() ) {
+                    for (InventoryItem item : shop.getItems()) {
 
                         if (item.isDynamic()) {
                             float itemChance = randNum.nextFloat();
-                            if (item.isNormalized() && itemChance >= (100-dynamicChance) / 100) {
+                            if (item.isNormalized() && itemChance >= (100 - dynamicChance) / 100) {
                                 /*
-                                 * Reduce price if:
-                                 * dynamicChance + half the value to 100 is greater than random var
+                                 * Reduce price if: dynamicChance + half the
+                                 * value to 100 is greater than random var
                                  * 
                                  * example: 75 + (100-75)/2 = 88
                                  */
-                                if (itemChance < dynamicChance+((100-dynamicChance)/2) ) {
-                                    //Get change percentage as expressed in decimal
-                                    int change = 1 - ((randGen(maxPriceChange-minPriceChange)+minPriceChange)/100);
-                                    item.setDynBuyPrice(item.getBuyPrice()*change);
-                                    item.setDynSellPrice(item.getSellPrice()*change);
+                                if (itemChance < dynamicChance + ((100 - dynamicChance) / 2)) {
+                                    // Get change percentage as expressed in
+                                    // decimal
+                                    int change = 1 - ((randGen(maxPriceChange - minPriceChange) + minPriceChange) / 100);
+                                    item.setDynBuyPrice(item.getBuyPrice() * change);
+                                    item.setDynSellPrice(item.getSellPrice() * change);
                                 }
-                                //Increase price
+                                // Increase price
                                 else {
-                                    //Get change percentage as expressed in decimal
-                                    int change = 1 + ((randGen(maxPriceChange-minPriceChange)+minPriceChange)/100);
-                                    item.setDynBuyPrice(item.getBuyPrice()*change);
-                                    item.setDynSellPrice(item.getSellPrice()*change);
+                                    // Get change percentage as expressed in
+                                    // decimal
+                                    int change = 1 + ((randGen(maxPriceChange - minPriceChange) + minPriceChange) / 100);
+                                    item.setDynBuyPrice(item.getBuyPrice() * change);
+                                    item.setDynSellPrice(item.getSellPrice() * change);
                                 }
-                            }
-                            else if (itemChance >= (100-dynamicChance) / 100){
-                                //Reset prices to normal
+                            } else if (itemChance >= (100 - dynamicChance) / 100) {
+                                // Reset prices to normal
                                 item.setDynBuyPrice(item.getBuyPrice());
                                 item.setDynSellPrice(item.getSellPrice());
                             }
                         }
-                    }  
+                    }
                 }
             }
 
@@ -115,9 +108,10 @@ public class DynamicThread extends Thread {
      * Generate a random number between 1 and max and return the result
      * 
      * @params int max - maximum number possible
+     * 
      * @returns int Result - generated random number between 1 and max
      */
-    private int randGen (int max) {
+    private int randGen(int max) {
         int result = randNum.nextInt(max) + 1;
         return result;
     }
