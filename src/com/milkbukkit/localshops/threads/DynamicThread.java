2d83e9318aa709401339ce99d3f1d4677fab9539
sleaker <sleaker@sleak-pc>
2011-05-25 15:08:53 -0700

    Imports? what imports? we don't need no imports!

--- src/com/milkbukkit/localshops/threads/DynamicThread.java
+++ src/com/milkbukkit/localshops/threads/DynamicThread.java
@@ -1,13 +1,6 @@
 package com.milkbukkit.localshops.threads;
 
-import java.util.Random;
-import java.util.logging.Logger;
-
-import com.milkbukkit.localshops.Config;
-import com.milkbukkit.localshops.DynamicManager;
-import com.milkbukkit.localshops.InventoryItem;
 import com.milkbukkit.localshops.LocalShops;
-import com.milkbukkit.localshops.Shop;
 
 /*
  * Represents the Dynamic Shop Task which is run in a thread
