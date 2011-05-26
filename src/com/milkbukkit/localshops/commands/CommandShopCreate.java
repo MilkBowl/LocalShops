5f25b543acf1c2099c005ea166d2be87b20b38fe
sleaker <sleaker@sleak-pc>
2011-05-26 08:19:32 -0700

    Actually assign the new pattern to the pattern var. herp derp!

--- src/com/milkbukkit/localshops/commands/CommandShopCreate.java
+++ src/com/milkbukkit/localshops/commands/CommandShopCreate.java
@@ -31,7 +31,7 @@ public class CommandShopCreate extends Command {
         String world = null;
         int[] xyzA = new int[3];
         int[] xyzB = new int[3];
-        
+
         Player player = null;
 
         // Get current shop
@@ -121,14 +121,14 @@ public class CommandShopCreate extends Command {
             }
         }
 
-        if (name == null) {
-            matcher.reset();
-            Pattern.compile("(?i)create\\s+(.*)");
-            matcher = pattern.matcher(command);
-            if (matcher.find()) {
-                name = matcher.group(1);
-            }
+
+        matcher.reset();
+        pattern = Pattern.compile("(?i)create\\s+(.*)");
+        matcher = pattern.matcher(command);
+        if (matcher.find()) {
+            name = matcher.group(1);
         }
+
 
         Shop shop = new Shop(UUID.randomUUID());
         shop.setCreator(creator);
